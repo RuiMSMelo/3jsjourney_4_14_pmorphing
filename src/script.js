17e916fba0cdcd17f0eabc77227cd98f099dc5d9
RuiMSMelo <ruimsmelo@gmail.com>
2025-05-08 00:01:49 +0100

    transition and buttons working

--- src/script.js
+++ src/script.js
@@ -30,35 +30,44 @@ gltfLoader.setDRACOLoader(dracoLoader)
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight,
-    pixelRatio: Math.min(window.devicePixelRatio, 2)
+  width: window.innerWidth,
+  height: window.innerHeight,
+  pixelRatio: Math.min(window.devicePixelRatio, 2),
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-    sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
 
-    // Materials
-    particles.material.uniforms.uResolution.value.set(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)
+  // Materials
+  if (particles) {
+    particles.material.uniforms.uResolution.value.set(
+      sizes.width * sizes.pixelRatio,
+      sizes.height * sizes.pixelRatio
+    )
+  }
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(sizes.pixelRatio)
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(sizes.pixelRatio)
 })
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(
+  35,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
 camera.position.set(0, 0, 8 * 2)
 scene.add(camera)
 
@@ -70,53 +79,147 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true,
+  canvas: canvas,
+  antialias: true,
 })
 
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(sizes.pixelRatio)
 
 debugObject.clearColor = '#160920'
-gui.addColor(debugObject, 'clearColor').onChange(() => { renderer.setClearColor(debugObject.clearColor) })
+gui.addColor(debugObject, 'clearColor').onChange(() => {
+  renderer.setClearColor(debugObject.clearColor)
+})
 renderer.setClearColor(debugObject.clearColor)
 
 /**
  * Particles
  */
-const particles = {}
+// Load models
+let particles = null
+gltfLoader.load('./models.glb', (gltf) => {
+  particles = {}
+  particles.index = 0
 
-// Geometry
-particles.geometry = new THREE.SphereGeometry(3)
+  // Positions
+  const positions = gltf.scene.children.map((child) => {
+    return child.geometry.attributes.position
+  })
 
-// Material
-particles.material = new THREE.ShaderMaterial({
+  particles.maxCount = 0
+  for (const position of positions) {
+    if (position.count > particles.maxCount) particles.maxCount = position.count
+  }
+
+  particles.positions = []
+  for (const position of positions) {
+    const originalArray = position.array
+    const newArray = new Float32Array(particles.maxCount * 3)
+
+    for (let i = 0; i < particles.maxCount; i++) {
+      const i3 = i * 3
+      if (i3 < originalArray.length) {
+        newArray[i3 + 0] = originalArray[i3 + 0]
+        newArray[i3 + 1] = originalArray[i3 + 1]
+        newArray[i3 + 2] = originalArray[i3 + 2]
+      } else {
+        const randomIndex = Math.floor(position.count * Math.random()) * 3
+        newArray[i3 + 0] = originalArray[randomIndex + 0]
+        newArray[i3 + 1] = originalArray[randomIndex + 1]
+        newArray[i3 + 2] = originalArray[randomIndex + 2]
+      }
+    }
+
+    particles.positions.push(new THREE.Float32BufferAttribute(newArray, 3))
+  }
+
+  // Geometry
+  particles.geometry = new THREE.BufferGeometry()
+  particles.geometry.setAttribute(
+    'position',
+    particles.positions[particles.index]
+  )
+  particles.geometry.setAttribute('aPositionTarget', particles.positions[3])
+
+  // Material
+  particles.material = new THREE.ShaderMaterial({
     vertexShader: particlesVertexShader,
     fragmentShader: particlesFragmentShader,
-    uniforms:
-    {
-        uSize: new THREE.Uniform(0.4),
-        uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio))
-    }
-})
+    uniforms: {
+      uSize: new THREE.Uniform(0.2),
+      uResolution: new THREE.Uniform(
+        new THREE.Vector2(
+          sizes.width * sizes.pixelRatio,
+          sizes.height * sizes.pixelRatio
+        )
+      ),
+      uProgress: new THREE.Uniform(0),
+    },
+    blending: THREE.AdditiveBlending,
+    depthWrite: false,
+  })
 
-// Points
-particles.points = new THREE.Points(particles.geometry, particles.material)
-scene.add(particles.points)
+  // Points
+  particles.points = new THREE.Points(particles.geometry, particles.material)
+  scene.add(particles.points)
+
+  // Methods
+  particles.morph = (index) => {
+    // Update attributes
+    particles.geometry.attributes.position =
+      particles.positions[particles.index]
+    particles.geometry.attributes.aPositionTarget = particles.positions[index]
+
+    // Animate uProgress
+    gsap.fromTo(
+      particles.material.uniforms.uProgress,
+      { value: 0 },
+      { value: 1, duration: 2, ease: 'linear' }
+    )
+
+    // Save index
+    particles.index = index
+  }
+  particles.morph0 = () => {
+    particles.morph(0)
+  }
+  particles.morph1 = () => {
+    particles.morph(1)
+  }
+  particles.morph2 = () => {
+    particles.morph(2)
+  }
+  particles.morph3 = () => {
+    particles.morph(3)
+  }
+
+  // Tweaks
+  gui
+    .add(particles.material.uniforms.uProgress, 'value')
+    .min(0)
+    .max(1)
+    .step(0.001)
+    .name('uProgress')
+    .listen()
+
+  gui.add(particles, 'morph0')
+  gui.add(particles, 'morph1')
+  gui.add(particles, 'morph2')
+  gui.add(particles, 'morph3')
+})
 
 /**
  * Animate
  */
-const tick = () =>
-{
-    // Update controls
-    controls.update()
+const tick = () => {
+  // Update controls
+  controls.update()
 
-    // Render normal scene
-    renderer.render(scene, camera)
+  // Render normal scene
+  renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
